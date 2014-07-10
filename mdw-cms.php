c3186c303fe6b4b9418b599910615a4501673c1f
erikdmitchell <erikdmitchell@gmail.com>
2014-07-10 12:04:54 -0400

    Added our updater class/functionality to the plugin. All future versions will be able to be upgrade via WordPress.

--- mdw-cms.php
+++ mdw-cms.php
@@ -18,6 +18,7 @@ require_once(plugin_dir_path(__FILE__).'inc/custom-widgets.php');
 require_once(plugin_dir_path(__FILE__).'admin-page.php');
 require_once(plugin_dir_path(__FILE__).'/classes/slider.php'); // our bootstrap slider
 require_once(plugin_dir_path(__FILE__).'/classes/social-media.php'); // our social media page
+require_once(plugin_dir_path(__FILE__).'/updater/updater.php'); // our bitbucket updater stuff
 
 require_once(plugin_dir_path(__FILE__).'mdw-cms-demo.php'); // our demo setup stuff
 
