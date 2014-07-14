e0c19f9e50d7de50c26868269c4f515db7cacf31
erikdmitchell <erikdmitchell@gmail.com>
2014-07-14 10:19:21 -0400

    Cleaned up sample file.

--- mdw-cms-config-sample.php
+++ mdw-cms-config-sample.php
@@ -17,29 +17,6 @@ $mdw_custom_post_types->add_post_types($args);
 // add custom taxonomy //
 $mdw_custom_taxonomies->add_taxonomy('supplier-type','suppliers','Supplier Type');
 
-// custom meta boxes for suppliers //
-/*
-$config=array(
-	'id' => 'supplier_meta',
-	'title' => 'Supplier Details',
-	'prefix' => 'supplier',
-	'post_types' => 'suppliers',
-	'duplicate' => 1,
-);
-$suppliers_meta=new mdw_Meta_Box($config);
-
-$suppliers_meta->add_field(array(
-	'id' => 'address',
-	'type' => 'textarea',
-	'label' => 'Address'
-));
-$suppliers_meta->add_field(array(
-	'id' => 'url',
-	'type' => 'text',
-	'label' => 'URL'
-));
-*/
-
 // custom meta boxes for meats //
 $config=array(
 	array(
@@ -126,51 +103,3 @@ $params=array(
 );
 
 new MDW_Widget_Creator($params);
-/*
-// meta boxes //
-$config=array(
-	'id' => 'sample_mb_id',
-	'title' => 'Sample Meta Box',
-	// 'prefix' => '', //
-	'post_types' => 'page'
-);
-$sample_meta_box = new mdw_Meta_Box($config);
-
-$sample_meta_box->add_field(array(
-	'id' => 'sample'
-));
-$sample_meta_box->add_field(array(
-	'id' => 'sample-text',
-	'type' => 'text',
-));
-$sample_meta_box->add_field(array(
-	'id' => 'sample-checkbox',
-	'type' => 'checkbox',
-	'label' => 'Checkbox',
-));		
-$sample_meta_box->add_field(array(
-	'id' => 'sample-textarea',
-	'type' => 'textarea',
-	'label' => 'Textarea',
-));	
-$sample_meta_box->add_field(array(
-	'id' => 'sample-wysiwyg',
-	'type' => 'wysiwyg',
-	'label' => 'WYSIWYG',
-));	
-$sample_meta_box->add_field(array(
-	'id' => 'sample-media',
-	'type' => 'media',
-	'label' => 'Media',
-));	
-$sample_meta_box->add_field(array(
-	'id' => 'sample-media-video',
-	'type' => 'media',
-	'label' => 'Media - Video',
-));	
-$sample_meta_box->add_field(array(
-	'id' => 'sample-media-file',
-	'type' => 'media',
-	'label' => 'Media - File',
-));
-*/
