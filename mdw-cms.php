e4d1953d154709925df641b2ae78af10977427d4
erikdmitchell <erikdmitchell@gmail.com>
2015-02-02 12:30:58 -0500

    updated to version 1.1.2

--- mdw-cms.php
+++ mdw-cms.php
@@ -2,18 +2,15 @@
 /*
 Plugin Name: MDW CMS
 Description: Adds cusomtized functionality to the site to make WordPress super awesome.
-Version: 1.1.1
+Version: 1.1.2
 Author: MillerDesignworks
 Author URI: http://www.millerdesignworks.com
 License: GPL2
 @erikdmitchell
 */
 
-//require_once(plugin_dir_path(__FILE__).'inc/mdw-custom-post-types.php');
 require_once(plugin_dir_path(__FILE__).'inc/mdw-custom-tax.php');
 require_once(plugin_dir_path(__FILE__).'inc/admin-columns.php');
-//require_once(plugin_dir_path(__FILE__).'inc/mdw-meta-boxes/mdwmb-plugin.php');
-//require_once(plugin_dir_path(__FILE__).'inc/mdw-meta-boxes/ajax-meta-boxes.php'); // may roll into mdwmd-plugin
 require_once(plugin_dir_path(__FILE__).'inc/custom-widgets.php');
 require_once(plugin_dir_path(__FILE__).'admin-page.php');
 
@@ -33,40 +30,35 @@ $admin_notices=array();
 
 /**
  * look for a config file in the plugins dir, this is utalized if it exists and is not over written by the plugin
- */
-/**
  * to prevent over writes, we use a wp option now
  * version 1.1.0 -- Not used due to previous setup - will be integrated later
  */
-//add_action('wp_head','mdwcms_loaded');
-//function mdwcms_loaded() {
-	global $admin_notices;
-
-	if (get_option('mdw_cms_version')) :		
+if (get_option('mdw_cms_version')) :
+	$admin_notices[]=array(
+		'class' => 'updated',
+		'message' => 'MDW CMS Version '.get_option('mdw_cms_version').' is in use.'
+	);
+else :
+	// we need to do a legacy update //
+	if (file_exists(plugin_dir_path(dirname(__FILE__)).'mdw-cms-config.php')) :
 		$admin_notices[]=array(
-			'class' => 'updated',
-			'message' => 'MDW CMS Version '.get_option('mdw_cms_version').' is in use.'
+			'class' => 'error',
+			'message' => 'MDW CMS is currently using a custom config file. Please update to the latest version of the plugin.'
 		);
-	else :		
-		// we need to do a legacy update //
-		if (file_exists(plugin_dir_path(dirname(__FILE__)).'mdw-cms-config.php')) :
-			$admin_notices[]=array(
-				'class' => 'error',
-				'message' => 'MDW CMS is currently using a custom config file. Please update to the latest version of the plugin.'
-			);
-			require_once(plugin_dir_path(dirname(__FILE__)).'mdw-cms-config.php');
-		else :
-			// 	we dont need to do anything //
-			$admin_notices[]=array(
-				'class' => 'error',
-				'message' => 'MDW CMS is currently using the default config file. Please update to the latest version of the plugin.'
-			);			
-			require_once(plugin_dir_path(__FILE__).'mdw-cms-config-sample.php');
-			
-			// update_option('mdw_cms_version',$this->version);
-		endif;
+		require_once(plugin_dir_path(dirname(__FILE__)).'mdw-cms-config.php');
+		MDWCMSlegacy::legacy_remove_old_config_file(plugin_dir_path(dirname(__FILE__)).'mdw-cms-config.php');
+	else :
+		// 	we dont need to do anything //
+		$admin_notices[]=array(
+			'class' => 'error',
+			'message' => 'MDW CMS is currently using the default config file. Please update to the latest version of the plugin.'
+		);
+		require_once(plugin_dir_path(__FILE__).'mdw-cms-config-sample.php');
+		MDWCMSlegacy::legacy_remove_old_config_file(plugin_dir_path(__FILE__).'mdw-cms-config-sample.php');
+		// update_option('mdw_cms_version',$this->version);
 	endif;
-//}
+endif;
+
 
 /**
  * displays our admin notices for the legacy support/migration of older content
@@ -74,13 +66,13 @@ $admin_notices=array();
 add_action('admin_notices','mdwcms_admin_notices');
 function mdwcms_admin_notices() {
 	global $admin_notices;
-	
+
 	$html=null;
-	
+
 	foreach ($admin_notices as $notice) :
 		$html.='<div class="'.$notice['class'].'"><p>'.$notice['message'].'</p></div>';
 	endforeach;
-	
+
 	echo $html;
 }
 
