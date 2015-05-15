6c066d86357cac42236f4aeeadfc38242955ccdd
Marshall Oram <oram@millerdesignworks.com>
2015-05-15 16:55:32 -0400

    Add Content widget as a unility widget for pulling a page or post content into a widget area
    lots of options useful for building home page features.

--- mdw-cms.php
+++ mdw-cms.php
@@ -21,6 +21,7 @@ require_once(plugin_dir_path(__FILE__).'classes/custom-taxonomy.php'); // calls 
 require_once(plugin_dir_path(__FILE__).'classes/custom-post-types.php'); // calls custom post types
 require_once(plugin_dir_path(__FILE__).'classes/social-media.php'); // our social media page
 require_once(plugin_dir_path(__FILE__).'classes/inflector.php'); // our pluralizing/singular functions
+require_once(plugin_dir_path(__FILE__).'widgets/widgets.php'); // some basic utility widgets
 
 /**
  * runs our update functions
