8355f8db94cab8ae7e3aac2e331756621bb77345
erikdmitchell <erikdmitchell@gmail.com>
2016-04-29 14:01:53 -0400

    removed bs styling from cpt page

--- admin/pages/custom-post-types.php
+++ admin/pages/custom-post-types.php
@@ -32,133 +32,170 @@
 		$btn_text='Update';
 ?>
 
+<h3>Add New Custom Post Type</h3>
 
-<div class="row">
+<form class="custom-post-types" method="post">
+	<?php wp_nonce_field('update_cpts', 'mdw_cms_admin'); ?>
 
-	<form class="custom-post-types col-md-8" method="post">
-		<h3>Add New Custom Post Type</h3>
-		<div class="form-row row">
-			<label for="name" class="required ">Post Type Name</label>
-			<div class="input">
-				<input type="text" name="name" id="name" value="<?php echo $name; ?>" />
-			</div>
-			<span class="description">(e.g. movie)</span>
-			<div id="mdw-cms-name-error" class="<?php echo $error_class; ?>"></div>
-			<div class="description-ext">Max 20 characters, can not contain capital letters or spaces. Reserved post types: post, page, attachment, revision, nav_menu_item.</div>
-		</div>
+	<table class="form-table">
+		<tbody>
 
-		<div class="form-row row">
-			<label for="label" class="">Label</label>
-			<div class="input">
-				<input type="text" name="label" id="label" value="<?php echo $label; ?>" />
-			</div>
-			<span class="description">(e.g. Movies)</span>
-		</div>
+			<tr>
+				<th scope="row">
+					<label for="name" class="required ">Post Type Name</label>
+				</th>
+				<td>
+					<input type="text" name="name" id="name" value="<?php echo $name; ?>" /><span class="example">(e.g. movie)</span>
+					<div id="mdw-cms-name-error" class="<?php echo $error_class; ?>"></div>
+					<p class="description">
+						Max 20 characters, can not contain capital letters or spaces. Reserved post types: post, page, attachment, revision, nav_menu_item.
+					</p>
+				</td>
+			</tr>
 
-		<div class="form-row row">
-			<label for="singular_label" class="">Singular Label</label>
-			<div class="input">
-				<input type="text" name="singular_label" id="singular_label" value="<?php echo $singular_label; ?>" />
-			</div>
-			<span class="description">(e.g. Movie)</span>
-		</div>
+			<tr>
+				<th scope="row">
+					<label for="label" class="">Label</label>
+				</th>
+				<td>
+					<input type="text" name="label" id="label" value="<?php echo $label; ?>" /><span class="example">(e.g. Movies)</span>
+				</td>
+			</tr>
 
-		<div class="form-row row">
-			<label for="description" class="">Description</label>
-			<textarea name="description" id="description" rows="4" cols="40"><?php echo $description; ?></textarea>
-		</div>
+			<tr>
+				<th scope="row">
+					<label for="singular_label" class="">Singular Label</label>
+				</th>
+				<td>
+					<input type="text" name="singular_label" id="singular_label" value="<?php echo $singular_label; ?>" /><span class="example">(e.g. Movie)</span>
+				</td>
+			</tr>
 
-		<div class="advanced-options">
-			<div class="form-row row">
-				<label for="title" class="">Title</label>
-				<div class="">
+			<tr>
+				<th scope="row">
+					<label for="description" class="">Description</label>
+				</th>
+				<td>
+					<textarea name="description" id="description" rows="4" cols="40"><?php echo $description; ?></textarea>
+				</td>
+			</tr>
+
+			<tr>
+				<th scope="row">
+					<label for="title" class="">Title</label>
+				</th>
+				<td>
 					<select name="title" id="title">
 						<option value="1" <?php selected ($title,1,false); ?>>True</option>
 						<option value="0" <?php selected ($title,0,false); ?>>False</option>
 					</select>
-				</div>
-				<span class="description">(default True)</span>
-			</div>
-			<div class="form-row row">
-				<label for="thumbnail" class="">Thumbnail</label>
-				<div class="">
+					<span class="example">(default True)</span>
+				</td>
+			</tr>
+
+			<tr>
+				<th scope="row">
+					<label for="thumbnail" class="">Thumbnail</label>
+				</th>
+				<td>
 					<select name="thumbnail" id="thumbnaill">
 						<option value="1" <?php selected ($thumbnail,1,false); ?>>True</option>
 						<option value="0" <?php selected ($thumbnail,0,false); ?>>False</option>
 					</select>
-				</div>
-				<span class="description">(default True)</span>
-			</div>
-			<div class="form-row row">
-				<label for="editor" class="">Editor</label>
-				<div class="">
-					<select name="editor" id="editor" >
-						<option value="1" <?php selected ($editor,1,false); ?>>True</option>
-						<option value="0" <?php selected ($editor,0,false); ?>>False</option>
-					</select>
-				</div>
-				<span class="description">(default True)</span>
-			</div>
-			<div class="form-row row">
-				<label for="revisions" class="">Revisions</label>
-				<div class="">
+					<span class="example">(default True)</span>
+				</td>
+			</tr>
+
+			<tr>
+				<th scope="row">
+					<label for="editor" class="">Editor</label>
+				</th>
+				<td>
+				<select name="editor" id="editor" >
+					<option value="1" <?php selected ($editor,1,false); ?>>True</option>
+					<option value="0" <?php selected ($editor,0,false); ?>>False</option>
+				</select>
+				<span class="example">(default True)</span>
+				</td>
+			</tr>
+
+			<tr>
+				<th scope="row">
+					<label for="revisions" class="">Revisions</label>
+				</th>
+				<td>
 					<select name="revisions" id="revisions">
 						<option value="1" <?php selected ($revisions,1,false); ?>>True</option>
 						<option value="0" <?php selected ($revisions,0,false); ?>>False</option>
 					</select>
-				</div>
-				<span class="description">(default True)</span>
-			</div>
-			<div class="form-row row">
-				<label for="hierarchical" class="">Hierarchical</label>
-				<div class="">
+					<span class="example">(default True)</span>
+				</td>
+			</tr>
+
+			<tr>
+				<th scope="row">
+					<label for="hierarchical" class="">Hierarchical</label>
+				</th>
+				<td>
 					<select name="hierarchical" id="hierarchical">
 						<option value="1" <?php selected ($hierarchical,1,false); ?>>True</option>
 						<option value="0" <?php selected ($hierarchical,0,false); ?>>False</option>
 					</select>
-				</div>
-				<span class="description">(default False)</span>
-				<div class="description-ext">Whether the post type is hierarchical (e.g. page). Allows Parent to be specified. Note: "page-attributes" must be set to true to show the parent select box.</div>
-			</div>
+					<span class="example">(default False)</span>
+					<p class="description">
+						Whether the post type is hierarchical (e.g. page). Allows Parent to be specified. Note: "page-attributes" must be set to true to show
+						the parent select box.
+					</p>
+				</td>
+			</tr>
 
-			<div class="form-row row">
-				<label for="page_attributes" class="">Page Attributes</label>
-				<div class="">
+			<tr>
+				<th scope="row">
+					<label for="page_attributes" class="">Page Attributes</label>
+				</th>
+				<td>
 					<select name="page_attributes" id="page_attributes">
 						<option value="1" <?php selected ($page_attributes,1,false); ?>>True</option>
 						<option value="0" <?php selected ($page_attributes,0,false); ?>>False</option>
 					</select>
-				</div>
-				<span class="description">(default False)</span>
-			</div>
 
-			<div class="form-row row">
-				<label for="comments" class=""><?php echo __('Comments'); ?></label>
-				<div class="">
+					<span class="example">(default False)</span>
+				</td>
+			</tr>
+
+			<tr>
+				<th scope="row">
+					<label for="comments" class=""><?php echo __('Comments'); ?></label>
+				</th>
+				<td>
 					<select name="comments" id="comments">
 						<option value="1" <?php selected ($comments,1,false); ?>>True</option>
 						<option value="0" <?php selected ($comments,0,false); ?>>False</option>
 					</select>
-				</div>
-				<span class="description">(default False)</span>
+
+					<span class="example">(default False)</span>
+				</td>
+			</tr>
+
+		</tbody>
+	</table>
+
+	<p class="submit">
+		<input type="submit" name="add-cpt" id="submit" class="button button-primary" value="<?php echo $btn_text; ?>" <?php echo $btn_disabled; ?>>
+	</p>
+
+	<input type="hidden" name="cpt-id" id="cpt-id" value=<?php echo $id; ?> />
+</form>
+
+<div class="custom-post-types-list">
+	<h3>Custom Post Types</h3>
+
+	<?php if ($mdw_cms_admin->options['post_types']) : ?>
+		<?php foreach ($mdw_cms_admin->options['post_types'] as $cpt) : ?>
+			<div class="cpt-row">
+				<span class="cpt>"><?php echo $cpt['label']; ?></span><span class="edit">[<a href="<?php echo $base_url; ?>&edit=cpt&slug=<?php echo $cpt['name']; ?>">Edit</a>]</span><span class="delete">[<a href="<?php echo $base_url; ?>&delete=cpt&slug=<?php echo $cpt['name']; ?>">Delete</a>]</span>
 			</div>
+		<?php endforeach; ?>
+	<?php endif; ?>
 
-		</div>
-		<p class="submit"><input type="submit" name="add-cpt" id="submit" class="button button-primary" value="<?php echo $btn_text; ?>" <?php echo $btn_disabled; ?>></p>
-		<input type="hidden" name="cpt-id" id="cpt-id" value=<?php echo $id; ?> />
-	</form>
-
-	<div class="custom-post-types-list col-md-4">
-		<h3>Custom Post Types</h3>
-
-		<?php if ($mdw_cms_admin->options['post_types']) : ?>
-			<?php foreach ($mdw_cms_admin->options['post_types'] as $cpt) : ?>
-				<div class="cpt-row row">
-					<span class="cpt>"><?php echo $cpt['label']; ?></span><span class="edit">[<a href="<?php echo $base_url; ?>&edit=cpt&slug=<?php echo $cpt['name']; ?>">Edit</a>]</span><span class="delete">[<a href="<?php echo $base_url; ?>&delete=cpt&slug=<?php echo $cpt['name']; ?>">Delete</a>]</span>
-				</div>
-			<?php endforeach; ?>
-		<?php endif; ?>
-
-	</div>
-
-</div><!-- .row -->
+</div>
