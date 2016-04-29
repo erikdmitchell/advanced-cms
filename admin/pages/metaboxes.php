0c8044f4cf75084124a9d97c2830da1ae77035f5
erikdmitchell <erikdmitchell@gmail.com>
2016-04-29 14:16:29 -0400

    layout for metaboxes fixed

--- admin/pages/metaboxes.php
+++ admin/pages/metaboxes.php
@@ -25,70 +25,78 @@
 	endif;
 ?>
 
-
 <h3>Add Metabox</h3>
 
-<form class="custom-metabox col-md-8" method="post">
+<form class="custom-metabox" method="post">
+	<?php wp_nonce_field('upadte_taxonomies', 'mdw_cms_admin'); ?>
 
-	<div class="form-row row">
-		<label for="mb_id" class="required ">Metabox ID</label>
-		<div class="input">
-			<input type="text" name="mb_id" id="mb_id" class="" value="<?php echo $mb_id; ?>" />
-		</div>
-		<span class="description">(e.g. movie_details)</span>
-		<div class="mdw-cms-name-error col-md-6 col-md-offset-3"></div>
-	</div>
+	<table class="form-table">
+		<tbody>
 
-	<div class="form-row row">
-		<label for="title" class="">Title</label>
-		<div class="input">
-			<input type="text" name="title" id="title" class="" value="<?php echo $title; ?>" />
-		</div>
-		<span class="description">(e.g. Movie Details)</span>
-	</div>
+			<tr>
+				<th scope="row">
+					<label for="mb_id" class="required ">Metabox ID</label>
+				</th>
+				<td>
+					<input type="text" name="mb_id" id="mb_id" class="" value="<?php echo $mb_id; ?>" /><span class="example">(e.g. movie_details)</span>
+					<div class="mdw-cms-name-error" class=""></div>
+				</td>
+			</tr>
 
-	<div class="form-row row">
-		<label for="prefix" class="">Prefix</label>
-		<div class="input">
-			<input type="text" name="prefix" id="prefix" class="" value="<?php echo $prefix; ?>" />
-		</div>
-		<span class="description">(e.g. movies)</span>
-	</div>
+			<tr>
+				<th scope="row">
+					<label for="title" class="">Title</label>
+				</th>
+				<td>
+					<input type="text" name="title" id="title" class="" value="<?php echo $title; ?>" /><span class="example">(e.g. Movie Details)</span>
+				</td>
+			</tr>
 
-	<?php echo $mdw_cms_admin->get_post_types_list($post_types); ?>
+			<tr>
+				<th scope="row">
+					<label for="prefix" class="">Prefix</label>
+				</th>
+				<td>
+					<input type="text" name="prefix" id="prefix" class="" value="<?php echo $prefix; ?>" /><span class="example">(e.g. movies)</span>
+				</td>
+			</tr>
 
-	<div class="add-fields sortable-div <?php echo $edit_class_v; ?>">
+			<?php echo $mdw_cms_admin->get_post_types_list($post_types); ?>
 
-		<h3>Metabox Fields</h3>
+			<tr>
+				<td class="add-fields sortable-div <?php echo $edit_class_v; ?>">
+					<h3>Metabox Fields</h3>
 
-		<?php if ($fields) : ?>
-			<?php foreach ($fields as $field_id => $field) : ?>
-				<?php echo $mdw_cms_admin->build_field_rows($field_id,$field,$field_counter); ?>
-				<?php $field_counter++; ?>
-			<?php endforeach; ?>
-		<?php endif; ?>
+					<?php if ($fields) : ?>
+						<?php foreach ($fields as $field_id => $field) : ?>
+							<?php echo $mdw_cms_admin->build_field_rows($field_id,$field,$field_counter); ?>
+							<?php $field_counter++; ?>
+						<?php endforeach; ?>
+					<?php endif; ?>
 
-		<?php if ($field_counter==0) : // 0 is default ie no fields exist ?>
-			<?php echo $mdw_cms_admin->build_field_rows($field_id,null,$field_counter); // add 'default' field // ?>
-		<?php endif; ?>
-
-	</div><!-- .add-fields -->
+					<?php if ($field_counter==0) : // 0 is default ie no fields exist ?>
+						<?php echo $mdw_cms_admin->build_field_rows($field_id,null,$field_counter); // add 'default' field // ?>
+					<?php endif; ?>
+				</td>
+			</tr>
+		</tbody>
+	</table>
 
 	<p class="submit">
-		<input type="submit" name="update-metabox" id="submit" class="button button-primary" value="<?php echo $btn_text; ?>s">
+		<input type="submit" name="update-metabox" id="submit" class="button button-primary" value="<?php echo $btn_text; ?>">
 		<input type="button" name="add-field" id="add-field-btn" class="button button-primary add-field" value="Add Field">
 	</p>
 </form>
 
-	<div class="custom-metabox-list col-md-4">
-		<h3>Custom Metaboxes</h3>
+<div class="custom-metabox-list">
+	<h3>Custom Metaboxes</h3>
 
-		<?php if ($mdw_cms_admin->options['metaboxes']) : ?>
-			<?php foreach ($mdw_cms_admin->options['metaboxes'] as $mb) : ?>
-				<div class="metabox-row row">
-					<span class="mb"><?php echo $mb['title']; ?></span><span class="edit">[<a href="<?php echo $base_url;?>&edit=mb&mb_id=<?php echo $mb['mb_id']; ?>">Edit</a>]</span><span class="delete">[<a href="<?php echo $base_url; ?>&delete=mb&mb_id=<?php echo $mb['mb_id']; ?>">Delete</a>]</span>
-				</div>
-			<?php endforeach; ?>
-		<?php endif; ?>
+	<?php if ($mdw_cms_admin->options['metaboxes']) : ?>
+		<?php foreach ($mdw_cms_admin->options['metaboxes'] as $mb) : ?>
+			<div class="metabox-row">
+				<span class="mb"><?php echo $mb['title']; ?></span><span class="edit">[<a href="<?php echo $base_url;?>&edit=mb&mb_id=<?php echo $mb['mb_id']; ?>">Edit</a>]</span><span class="delete">[<a href="<?php echo $base_url; ?>&delete=mb&mb_id=<?php echo $mb['mb_id']; ?>">Delete</a>]</span>
+			</div>
+		<?php endforeach; ?>
+	<?php endif; ?>
 
-	</div>
+</div>
