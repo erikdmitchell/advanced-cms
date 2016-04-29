a04a1391a85ea2cfde91aaf5af4cb413c95f51a0
erikdmitchell <erikdmitchell@gmail.com>
2016-04-29 11:51:05 -0400

    added mdw_cms_get_template and admin pages folder

--- admin/functions.php
+++ admin/functions.php
@@ -46,4 +46,40 @@ function get_post_types_list($selected_pt=false,$output='checkbox') {
 
 	return $html;
 }
+
+/**
+ * mdw_cms_get_template function.
+ *
+ * @access public
+ * @param bool $template_name (default: false)
+ * @param mixed $attributes (default: null)
+ * @return void
+ */
+function mdw_cms_get_template($template_name=false, $attributes=null) {
+	if (!$attributes )
+		$attributes = array();
+
+	if (!$template_name)
+		return false;
+
+	ob_start();
+
+	do_action('ulm_before_'.$template_name);
+
+	if (file_exists(get_stylesheet_directory().'/ultimate-league-management/templates/'.$template_name.'.php')) :
+		include(get_stylesheet_directory().'/ultimate-league-management/templates/'.$template_name.'.php');
+	elseif (file_exists(get_template_directory().'/ultimate-league-management/templates/'.$template_name.'.php')) :
+		include(get_template_directory().'/ultimate-league-management/templates/'.$template_name.'.php');
+	else :
+		include(ULM_PATH.'/templates/'.$template_name.'.php');
+	endif;
+
+	do_action('ulm_after_'.$template_name);
+
+	$html=ob_get_contents();
+
+	ob_end_clean();
+
+	return $html;
+}
 ?>
