719994bb464aee4deeb28e88a277463e3ff355f4
erikdmitchell <erikdmitchell@gmail.com>
2016-04-29 11:44:36 -0400

    removed old dup metabox code
    it never worked and isn't worth keeping for now
    also removed coutry and state arrays - global vars now

--- inc/mdw-meta-boxes/mdwmb-plugin.php
+++ inc/mdw-meta-boxes/mdwmb-plugin.php
@@ -2,308 +2,12 @@
 class MDWMetaboxes {
 
 	private $nonce = 'wp_upm_media_nonce'; // Represents the nonce value used to save the post media //
-	private $version='1.2.6';
 	private $option_name='mdw_meta_box_duped_boxes';
 
 	protected $options=array();
 	protected $post_types=array();
 
 	public $fields=array();
-	public $countries=array(
-    "GB" => "United Kingdom",
-    "US" => "United States",
-    "AF" => "Afghanistan",
-    "AL" => "Albania",
-    "DZ" => "Algeria",
-    "AS" => "American Samoa",
-    "AD" => "Andorra",
-    "AO" => "Angola",
-    "AI" => "Anguilla",
-    "AQ" => "Antarctica",
-    "AG" => "Antigua And Barbuda",
-    "AR" => "Argentina",
-    "AM" => "Armenia",
-    "AW" => "Aruba",
-    "AU" => "Australia",
-    "AT" => "Austria",
-    "AZ" => "Azerbaijan",
-    "BS" => "Bahamas",
-    "BH" => "Bahrain",
-    "BD" => "Bangladesh",
-    "BB" => "Barbados",
-    "BY" => "Belarus",
-    "BE" => "Belgium",
-    "BZ" => "Belize",
-    "BJ" => "Benin",
-    "BM" => "Bermuda",
-    "BT" => "Bhutan",
-    "BO" => "Bolivia",
-    "BA" => "Bosnia And Herzegowina",
-    "BW" => "Botswana",
-    "BV" => "Bouvet Island",
-    "BR" => "Brazil",
-    "IO" => "British Indian Ocean Territory",
-    "BN" => "Brunei Darussalam",
-    "BG" => "Bulgaria",
-    "BF" => "Burkina Faso",
-    "BI" => "Burundi",
-    "KH" => "Cambodia",
-    "CM" => "Cameroon",
-    "CA" => "Canada",
-    "CV" => "Cape Verde",
-    "KY" => "Cayman Islands",
-    "CF" => "Central African Republic",
-    "TD" => "Chad",
-    "CL" => "Chile",
-    "CN" => "China",
-    "CX" => "Christmas Island",
-    "CC" => "Cocos (Keeling) Islands",
-    "CO" => "Colombia",
-    "KM" => "Comoros",
-    "CG" => "Congo",
-    "CD" => "Congo, The Democratic Republic Of The",
-    "CK" => "Cook Islands",
-    "CR" => "Costa Rica",
-    "CI" => "Cote D'Ivoire",
-    "HR" => "Croatia (Local Name: Hrvatska)",
-    "CU" => "Cuba",
-    "CY" => "Cyprus",
-    "CZ" => "Czech Republic",
-    "DK" => "Denmark",
-    "DJ" => "Djibouti",
-    "DM" => "Dominica",
-    "DO" => "Dominican Republic",
-    "TP" => "East Timor",
-    "EC" => "Ecuador",
-    "EG" => "Egypt",
-    "SV" => "El Salvador",
-    "GQ" => "Equatorial Guinea",
-    "ER" => "Eritrea",
-    "EE" => "Estonia",
-    "ET" => "Ethiopia",
-    "FK" => "Falkland Islands (Malvinas)",
-    "FO" => "Faroe Islands",
-    "FJ" => "Fiji",
-    "FI" => "Finland",
-    "FR" => "France",
-    "FX" => "France, Metropolitan",
-    "GF" => "French Guiana",
-    "PF" => "French Polynesia",
-    "TF" => "French Southern Territories",
-    "GA" => "Gabon",
-    "GM" => "Gambia",
-    "GE" => "Georgia",
-    "DE" => "Germany",
-    "GH" => "Ghana",
-    "GI" => "Gibraltar",
-    "GR" => "Greece",
-    "GL" => "Greenland",
-    "GD" => "Grenada",
-    "GP" => "Guadeloupe",
-    "GU" => "Guam",
-    "GT" => "Guatemala",
-    "GN" => "Guinea",
-    "GW" => "Guinea-Bissau",
-    "GY" => "Guyana",
-    "HT" => "Haiti",
-    "HM" => "Heard And Mc Donald Islands",
-    "VA" => "Holy See (Vatican City State)",
-    "HN" => "Honduras",
-    "HK" => "Hong Kong",
-    "HU" => "Hungary",
-    "IS" => "Iceland",
-    "IN" => "India",
-    "ID" => "Indonesia",
-    "IR" => "Iran (Islamic Republic Of)",
-    "IQ" => "Iraq",
-    "IE" => "Ireland",
-    "IL" => "Israel",
-    "IT" => "Italy",
-    "JM" => "Jamaica",
-    "JP" => "Japan",
-    "JO" => "Jordan",
-    "KZ" => "Kazakhstan",
-    "KE" => "Kenya",
-    "KI" => "Kiribati",
-    "KP" => "Korea, Democratic People's Republic Of",
-    "KR" => "Korea, Republic Of",
-    "KW" => "Kuwait",
-    "KG" => "Kyrgyzstan",
-    "LA" => "Lao People's Democratic Republic",
-    "LV" => "Latvia",
-    "LB" => "Lebanon",
-    "LS" => "Lesotho",
-    "LR" => "Liberia",
-    "LY" => "Libyan Arab Jamahiriya",
-    "LI" => "Liechtenstein",
-    "LT" => "Lithuania",
-    "LU" => "Luxembourg",
-    "MO" => "Macau",
-    "MK" => "Macedonia, Former Yugoslav Republic Of",
-    "MG" => "Madagascar",
-    "MW" => "Malawi",
-    "MY" => "Malaysia",
-    "MV" => "Maldives",
-    "ML" => "Mali",
-    "MT" => "Malta",
-    "MH" => "Marshall Islands",
-    "MQ" => "Martinique",
-    "MR" => "Mauritania",
-    "MU" => "Mauritius",
-    "YT" => "Mayotte",
-    "MX" => "Mexico",
-    "FM" => "Micronesia, Federated States Of",
-    "MD" => "Moldova, Republic Of",
-    "MC" => "Monaco",
-    "MN" => "Mongolia",
-    "MS" => "Montserrat",
-    "MA" => "Morocco",
-    "MZ" => "Mozambique",
-    "MM" => "Myanmar",
-    "NA" => "Namibia",
-    "NR" => "Nauru",
-    "NP" => "Nepal",
-    "NL" => "Netherlands",
-    "AN" => "Netherlands Antilles",
-    "NC" => "New Caledonia",
-    "NZ" => "New Zealand",
-    "NI" => "Nicaragua",
-    "NE" => "Niger",
-    "NG" => "Nigeria",
-    "NU" => "Niue",
-    "NF" => "Norfolk Island",
-    "MP" => "Northern Mariana Islands",
-    "NO" => "Norway",
-    "OM" => "Oman",
-    "PK" => "Pakistan",
-    "PW" => "Palau",
-    "PA" => "Panama",
-    "PG" => "Papua New Guinea",
-    "PY" => "Paraguay",
-    "PE" => "Peru",
-    "PH" => "Philippines",
-    "PN" => "Pitcairn",
-    "PL" => "Poland",
-    "PT" => "Portugal",
-    "PR" => "Puerto Rico",
-    "QA" => "Qatar",
-    "RE" => "Reunion",
-    "RO" => "Romania",
-    "RU" => "Russian Federation",
-    "RW" => "Rwanda",
-    "KN" => "Saint Kitts And Nevis",
-    "LC" => "Saint Lucia",
-    "VC" => "Saint Vincent And The Grenadines",
-    "WS" => "Samoa",
-    "SM" => "San Marino",
-    "ST" => "Sao Tome And Principe",
-    "SA" => "Saudi Arabia",
-    "SN" => "Senegal",
-    "SC" => "Seychelles",
-    "SL" => "Sierra Leone",
-    "SG" => "Singapore",
-    "SK" => "Slovakia (Slovak Republic)",
-    "SI" => "Slovenia",
-    "SB" => "Solomon Islands",
-    "SO" => "Somalia",
-    "ZA" => "South Africa",
-    "GS" => "South Georgia, South Sandwich Islands",
-    "ES" => "Spain",
-    "LK" => "Sri Lanka",
-    "SH" => "St. Helena",
-    "PM" => "St. Pierre And Miquelon",
-    "SD" => "Sudan",
-    "SR" => "Suriname",
-    "SJ" => "Svalbard And Jan Mayen Islands",
-    "SZ" => "Swaziland",
-    "SE" => "Sweden",
-    "CH" => "Switzerland",
-    "SY" => "Syrian Arab Republic",
-    "TW" => "Taiwan",
-    "TJ" => "Tajikistan",
-    "TZ" => "Tanzania, United Republic Of",
-    "TH" => "Thailand",
-    "TG" => "Togo",
-    "TK" => "Tokelau",
-    "TO" => "Tonga",
-    "TT" => "Trinidad And Tobago",
-    "TN" => "Tunisia",
-    "TR" => "Turkey",
-    "TM" => "Turkmenistan",
-    "TC" => "Turks And Caicos Islands",
-    "TV" => "Tuvalu",
-    "UG" => "Uganda",
-    "UA" => "Ukraine",
-    "AE" => "United Arab Emirates",
-    "UM" => "United States Minor Outlying Islands",
-    "UY" => "Uruguay",
-    "UZ" => "Uzbekistan",
-    "VU" => "Vanuatu",
-    "VE" => "Venezuela",
-    "VN" => "Viet Nam",
-    "VG" => "Virgin Islands (British)",
-    "VI" => "Virgin Islands (U.S.)",
-    "WF" => "Wallis And Futuna Islands",
-    "EH" => "Western Sahara",
-    "YE" => "Yemen",
-    "YU" => "Yugoslavia",
-    "ZM" => "Zambia",
-    "ZW" => "Zimbabwe"
-  );
-
-  public $states = array(
-	  'AL'=>'Alabama',
-	  'AK'=>'Alaska',
-	  'AZ'=>'Arizona',
-	  'AR'=>'Arkansas',
-	  'CA'=>'California',
-	  'CO'=>'Colorado',
-	  'CT'=>'Connecticut',
-	  'DE'=>'Delaware',
-	  'DC'=>'District of Columbia',
-	  'FL'=>'Florida',
-	  'GA'=>'Georgia',
-	  'HI'=>'Hawaii',
-	  'ID'=>'Idaho',
-	  'IL'=>'Illinois',
-	  'IN'=>'Indiana',
-	  'IA'=>'Iowa',
-	  'KS'=>'Kansas',
-	  'KY'=>'Kentucky',
-	  'LA'=>'Louisiana',
-	  'ME'=>'Maine',
-	  'MD'=>'Maryland',
-	  'MA'=>'Massachusetts',
-	  'MI'=>'Michigan',
-	  'MN'=>'Minnesota',
-	  'MS'=>'Mississippi',
-	  'MO'=>'Missouri',
-	  'MT'=>'Montana',
-	  'NE'=>'Nebraska',
-	  'NV'=>'Nevada',
-	  'NH'=>'New Hampshire',
-	  'NJ'=>'New Jersey',
-	  'NM'=>'New Mexico',
-	  'NY'=>'New York',
-	  'NC'=>'North Carolina',
-	  'ND'=>'North Dakota',
-	  'OH'=>'Ohio',
-	  'OK'=>'Oklahoma',
-	  'OR'=>'Oregon',
-	  'PA'=>'Pennsylvania',
-	  'RI'=>'Rhode Island',
-	  'SC'=>'South Carolina',
-	  'SD'=>'South Dakota',
-	  'TN'=>'Tennessee',
-	  'TX'=>'Texas',
-	  'UT'=>'Utah',
-	  'VT'=>'Vermont',
-	  'VA'=>'Virginia',
-	  'WA'=>'Washington',
-	  'WV'=>'West Virginia',
-	  'WI'=>'Wisconsin',
-	  'WY'=>'Wyoming',
-	);
 
 	/**
 	 * constructs our function, setups our scripts and styles, attaches meta box to wp actions
@@ -404,8 +108,6 @@ class MDWMetaboxes {
 		add_action('wp_enqueue_scripts',array($this,'register_scripts_styles'));
 		add_action('save_post',array($this,'save_custom_meta_data'));
 		add_action('add_meta_boxes',array($this,'mdwmb_add_meta_box'));
-		//add_action('wp_ajax_dup-box',array($this,'duplicate_meta_box'));
-		//add_action('wp_ajax_remove-box',array($this,'remove_duplicate_meta_box'));
 
 		add_action('wp_ajax_duplicate_metabox_field',array($this,'ajax_duplicate_metabox_field'));
 		add_action('wp_ajax_remove_duplicate_metabox_field',array($this,'ajax_remove_duplicate_metabox_field'));
@@ -1091,102 +793,6 @@ class MDWMetaboxes {
 		endforeach;
 	}
 
-	function duplicate_meta_box() {
-		$this->save_duplicate_meta_box($_POST['postID']);
-
-		exit;
-	}
-
-	function remove_duplicate_meta_box() {
-		$option=get_option($this->option_name);
-
-		// check for option key //
-		if (!isset($_POST['optionKey']))
-			return false;
-
-		$option_to_remove=$option[$_POST['optionKey']];
-
-		// do some quick checks to make sure all is ok //
-		if ($option_to_remove['post_id']!=$_POST['postID'])
-			return false;
-
-		if ($option_to_remove['id']!=$_POST['metaID'])
-			return false;
-
-		// remove post meta //
-		foreach ($option_to_remove['fields'] as $id => $field) :
-			$meta_key=$option_to_remove['prefix'].'_'.$id;
-			if ($meta_key)
-				delete_post_meta($_POST['postID'],$meta_key);
-		endforeach;
-
-		unset($option[$_POST['optionKey']]); // remove from option
-		$option=array_values($option); // reset keys
-
-		update_option($this->option_name,$option); // update our option
-
-		return true;
-
-		exit;
-	}
-
-	/**
-	 * saves our meta field data when we are duplicating a field
-	 * the field needs to be saved so that our class can be updated with the apropriate fields
-	 * it's done via ajax, so the users should not see anything
-	**/
-	public function save_duplicate_meta_box($post_id) {
-		// Bail if we're doing an auto save
-		if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
-
-		// if our current user can't edit this post, bail
-		if (!current_user_can('edit_post',$post_id)) return;
-
-		$data=null;
-		$option=false;
-		$option_arr=array();
-		$current_option_arr=array();
-		$new_option_arr=array();
-		$prefix=$_POST['prefix'];
-
-		foreach ($_POST['fields'] as $id => $field) :
-			$field_id=$prefix.'_'.$id;
-			$data='';
-			//update_post_meta($post_id,$field_id,$data);
-		endforeach;
-
-		// build our option so that we know we have duped boxes //
-		$option=$this->option_name;
-		$option_arr=array(
-			'post_id' => $_POST['postID'],
-			'id' => $_POST['id'],
-			'title' => $_POST['title'],
-			'prefix' => $_POST['prefix'],
-			'post_types' => $_POST['post_types'],
-			'duplicate' => 0,
-			'fields' => $_POST['fields'],
-		);
-
-/*
-print_r($option_arr);
-
-		if ($option)
-			$current_option_arr=get_option($option);
-
-		if ($current_option_arr) :
-			array_push($option_arr,$current_option_arr);
-		else :
-			$new_option_arr[0]=$option_arr;
-			$option_arr=$new_option_arr;
-		endif;
-
-print_r($option_arr);
-
-		if ($option)
-			update_option($option,$option_arr);
-*/
-	}
-
 	/**
 	 * setup our config with defaults and adjusments
 	**/
@@ -1197,8 +803,7 @@ print_r($option_arr);
 			'title' => 'Default Meta Box',
 			'prefix' => '_mdwmb',
 			'post_types' => 'post,page',
-			//'duplicate' => 0,
-			//'fields' => array(), // for legacy support (pre 1.1.8)
+			'fields' => array(), // for legacy support (pre 1.1.8)
 			'post_fields' => array()
 		);
 
@@ -1218,27 +823,6 @@ print_r($option_arr);
 
 		return $configs;
 	}
-
-/*
-	function build_duplicated_boxes($post_id=false) {
-		if (!$post_id)
-			return false;
-
-		$option_arr=get_option($this->option_name);
-
-		if (!count($option_arr) || !$option_arr)
-			return false;
-
-		foreach ($option_arr as $option) :
-			if ($option['post_id']==$post_id) :
-				$option['removable']=true; // allows us to have a remove button
-				array_push($this->config,$option);
-			endif;
-		endforeach;
-
-		return;
-	}
-*/
 
 	/**
 	 * clean_special_chars function.
@@ -1484,11 +1068,13 @@ print_r($option_arr);
 	 * @return void
 	 */
 	public function get_countries_dropdown($name='country',$selected='US') {
+		global $mdw_cms_countries;
+
 		$html=null;
 
 		$html.='<select name="'.$name.'">';
 			$html.='<option value="0">Select Country</option>';
-			foreach ($this->countries as $id => $country) :
+			foreach ($mdw_cms_countries as $id => $country) :
 				$html.='<option value="'.$id.'" '.selected($selected,$id,false).'>'.$country.'</option>';
 			endforeach;
 		$html.='</select>';
