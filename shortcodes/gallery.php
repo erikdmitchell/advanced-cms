f1935820606f6e96ac3b5548947cd7590fcc0318
erikdmitchell <erikdmitchell@gmail.com>
2016-10-19 11:43:17 -0400

    built gallery output and template

--- shortcodes/gallery.php
+++ shortcodes/gallery.php
@@ -19,7 +19,7 @@ function mdw_cms_gallery_shortcode($atts) {
 
   $image_ids=get_post_meta($post->ID, $id, true);
 
-	$gallery=new MDWCMSGallery(array('image_ids' => $image_ids));
+	$gallery=new MDWCMSGallery(array('id' => $id, 'image_ids' => $image_ids));
 
   return mdw_cms_get_template_part('gallery');
 }
@@ -32,13 +32,13 @@ class MDWCMSGallery {
 
 	public $images;
 
-	public $query;
-
 	public $current_image=-1;
 
 	public $image_count=0;
 
 	public $image;
+
+	public $id;
 
 	/**
 	 * __construct function.
@@ -62,6 +62,7 @@ class MDWCMSGallery {
 	 */
 	public function default_query_vars() {
 		$array=array(
+			'id' => 'gallery',
 			'image_ids' => '',
 			'size' => 'full',
 		);
@@ -95,18 +96,29 @@ class MDWCMSGallery {
 	public function query($query='') {
 		$this->query_vars=$this->set_query_vars($query);
 
+		$this->set_gallery_id($this->query_vars['id']);
+
 		$this->get_images($this->query_vars);
 
 		return $this;
 	}
 
+	/**
+	 * get_images function.
+	 *
+	 * @access public
+	 * @param string $vars (default: '')
+	 * @return void
+	 */
 	public function get_images($vars='') {
 		$images=array();
 
-		foreach ($vars['image_ids'] as $image_id) :
+		foreach ($vars['image_ids'] as $key => $image_id) :
+
 			$metadata=wp_get_attachment_metadata($image_id);
 
 			$image=new stdClass();
+			$image->id='image-'.$key;
 			$image->image=wp_get_attachment_image($image_id, $vars['size']);
 			$image->caption=$metadata['image_meta']['caption'];
 
@@ -117,6 +129,17 @@ class MDWCMSGallery {
 		$this->image_count=count($images);
 
 		return $this->images;
+	}
+
+	/**
+	 * set_gallery_id function.
+	 *
+	 * @access public
+	 * @param mixed $id
+	 * @return void
+	 */
+	public function set_gallery_id($id) {
+		$this->id=$id;
 	}
 
 	/**
@@ -198,5 +221,143 @@ function mdw_cms_gallery_the_image() {
 	global $mdw_cms_gallery;
 
 	$mdw_cms_gallery->the_image();
+}
+
+/**
+ * mdw_cms_gallery_id function.
+ *
+ * @access public
+ * @return void
+ */
+function mdw_cms_gallery_id() {
+	echo 	mdw_cms_gallery_get_id();
+}
+
+/**
+ * mdw_cms_gallery_get_id function.
+ *
+ * @access public
+ * @return void
+ */
+function mdw_cms_gallery_get_id() {
+	global $mdw_cms_gallery;
+
+	return $mdw_cms_gallery->id;
+}
+
+/**
+ * mdw_cms_image_class function.
+ *
+ * @access public
+ * @param string $class (default: '')
+ * @param mixed $image_id (default: null)
+ * @return void
+ */
+function mdw_cms_image_class($class='', $image_id=null) {
+	echo join(' ', mdw_cms_get_image_class($class, $image_id));
+}
+
+/**
+ * mdw_cms_get_image_class function.
+ *
+ * @access public
+ * @param string $class (default: '')
+ * @param mixed $image_id (default: null)
+ * @return void
+ */
+function mdw_cms_get_image_class($class='', $image_id=null) {
+	global $mdw_cms_gallery, $mdw_cms_image;
+
+	$classes=array();
+
+	if ($class) :
+		if (!is_array($class))
+			$class=explode(',', $classe);
+
+		$classes=array_map('esc_attr', $class);
+	else :
+		$class=array();
+	endif;
+
+	if (!$image_id)
+		$image_id=$mdw_cms_image->id;
+
+	$classes[]=$image_id;
+	$classes[]='item';
+
+	if ($mdw_cms_gallery->current_image==0)
+		$classes[]='active';
+
+	$classes=array_map('esc_attr', $classes);
+
+	return array_unique($classes);
+}
+
+/**
+ * mdw_cms_gallery_image function.
+ *
+ * @access public
+ * @return void
+ */
+function mdw_cms_gallery_image() {
+	echo mdw_cms_gallery_get_image();
+}
+
+/**
+ * mdw_cms_gallery_get_image function.
+ *
+ * @access public
+ * @return void
+ */
+function mdw_cms_gallery_get_image() {
+	global $mdw_cms_image;
+
+	return $mdw_cms_image->image;
+}
+
+/**
+ * mdw_cms_gallery_image_caption function.
+ *
+ * @access public
+ * @return void
+ */
+function mdw_cms_gallery_image_caption() {
+	echo mdw_cms_gallery_get_image_caption();
+}
+
+/**
+ * mdw_cms_gallery_get_image_caption function.
+ *
+ * @access public
+ * @return void
+ */
+function mdw_cms_gallery_get_image_caption() {
+	global $mdw_cms_image;
+
+	return $mdw_cms_image->caption;
+}
+
+function mdw_cms_gallery_indicators() {
+	echo mdw_cms_gallery_get_indicators();
+}
+
+function mdw_cms_gallery_get_indicators() {
+	global $mdw_cms_gallery;
+
+	$html=null;
+
+	if (!$mdw_cms_gallery->image_count)
+		return false;
+
+
+	$html.='<ol class="carousel-indicators">';
+		foreach ($mdw_cms_gallery->images as $key => $image) :
+	    <li data-target="#mdw-cms-carousel-<?php mdw_cms_gallery_id(); ?>" data-slide-to="0" class="active"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+		endforeach;
+	$html.='</ol>';
+
+	return $html;
 }
 ?>
--- templates/gallery.php
+++ templates/gallery.php
@@ -1,40 +1,48 @@
-<?php $images=mdw_cms_setup_gallery_images($atts->image_ids); ?>
+<?php if (mdw_cms_gallery_have_images()) : ?>
 
-<?php //print_r($images); ?>
+	<div id="mdw-cms-carousel-<?php mdw_cms_gallery_id(); ?>" class="mdw-cms carousel slide" data-ride="carousel">
+
+	  <?php mdw_cms_gallery_indicators(); ?>
+
+		<div class="carousel-inner" role="listbox">
+			<?php while ( mdw_cms_gallery_have_images() ) : mdw_cms_gallery_the_image(); ?>
+
+		    <div class="<?php mdw_cms_image_class(); ?>">
+		      <?php mdw_cms_gallery_image(); ?>
+		      <div class="carousel-caption">
+		        <?php mdw_cms_gallery_image_caption(); ?>
+		      </div>
+		    </div>
+
+		  <?php endwhile; ?>
+		</div>
+
+	  <a class="left carousel-control" href="#mdw-cms-carousel-<?php mdw_cms_gallery_id(); ?>" role="button" data-slide="prev">
+	    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="right carousel-control" href="#mdw-cms-carousel-<?php mdw_cms_gallery_id(); ?>" role="button" data-slide="next">
+	    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+
+	</div>
+
+<?php else : ?>
+
+	no images
+
+<?php endif; ?>
+
+
+
+
+
+
+
 
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-<div id="mdw-cms-carousel-<?php echo $atts->id; ?>" class="mdw-cms carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-  </ol>
-
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-	  <?php foreach ($images as $image) : ?>
-	    <div class="item <?php mdw_cms_gallery_item_classes(); ?>">
-	      <?php echo $image->image; ?>
-	      <div class="carousel-caption">
-	        <?php echo $image->caption; ?>
-	      </div>
-	    </div>
-    <?php endforeach; ?>
-  </div>
-
-  <!-- Controls -->
-  <a class="left carousel-control" href="#mdw-cms-carousel-<?php echo $atts->id; ?>" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#mdw-cms-carousel-<?php echo $atts->id; ?>" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
