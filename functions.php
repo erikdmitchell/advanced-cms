62d6a4ac42bb8f8522cde01b63027d9ad2c9e414
erikdmitchell <erikdmitchell@gmail.com>
2016-10-19 10:43:18 -0400

    added atts to get template part

--- functions.php
+++ functions.php
@@ -1,11 +1,20 @@
 <?php
-function mdw_cms_get_template_part($template_name='') {
+
+/**
+ * mdw_cms_get_template_part function.
+ *
+ * @access public
+ * @param string $template_name (default: '')
+ * @param string $atts (default: '')
+ * @return void
+ */
+function mdw_cms_get_template_part($template_name='', $atts='') {
 	if (empty($template_name))
 		return false;
 
 	ob_start();
 
-	do_action('mdw_cms_get_template_part'.$template_name);
+	do_action('mdw_cms_get_template_part_'.$template_name);
 
 	if (file_exists(get_stylesheet_directory().'/mdw-cms/'.$template_name.'.php')) :
 		include(get_stylesheet_directory().'/mdw-cms/'.$template_name.'.php');
