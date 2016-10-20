96aa66861651d0958ad9b572ada2c4fb29a4adf5
Erik Mitchell <erikdmitchell@gmail.com>
2016-10-20 12:07:06 -0400

    added taxonomy id check js

---
+++ js/jquery.taxonomy-id-check.js
@@ -0,0 +1,37 @@
+/**
+ * validates our taxonomy id
+ *
+ * @since 2.1.9.1
+ */
+(function($) {
+	$.fn.taxonomyIDcheck=function($this, value, options) {
+		var settings=$.extend({
+			maxLength : 20,
+			reserved : [],
+			errorField : $('.mdw-cms-name-error')
+		}, options);
+console.log(settings);
+		settings.errorField.text(''); // clear errors
+
+		// strip spaces and set to lowercase //
+		value=value.replace(/\s+/g, '').toLowerCase();
+
+		$this.val(value); // set value to clean one
+
+		// check if name is reserved //
+		if ($.inArray(value,settings.reserved)!=-1) {
+			settings.errorField.text('Can not use a reserved post type.');
+			return false;
+		}
+
+		// check length //
+		if (value.length>settings.maxLength) {
+			settings.errorField.text('Name length is too long.');
+			return false;
+		}
+
+		settings.errorField.text(''); // clear errors
+
+		return true;
+	};
+}(jQuery));
