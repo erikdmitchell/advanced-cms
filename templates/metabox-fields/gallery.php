0e2d5e0b482ed64f2103519d0889011a6b88a3e9
erikdmitchell <erikdmitchell@gmail.com>
2016-11-01 15:08:50 -0400

    setup layout and styling for disable bs checkbox

--- templates/metabox-fields/gallery.php
+++ templates/metabox-fields/gallery.php
@@ -6,3 +6,11 @@
 	<input class="gallery-remove button" name="<?php echo $atts['id']; ?>_button" id="<?php echo $atts['id']; ?>_button" value="Remove Gallery" />
 	<input class="gallery-ids" type="hidden" name="<?php echo $atts['id']; ?>" value="<?php echo $MDWMetaboxes->get_gallery_image_ids($value); ?>" />
 </div>
+
+<div class="gallery-settings">
+	<label for="disable-bootstrap">Disable Bootstrap</label><input type="checkbox" id="disable-bootstrap" name="disable-bootstrap" value="" />
+	<div class="description">
+		If your theme has bootstrap, we recommend disabling our gallery bootstrap due to potential conflicts. We do not utilize, nor include all
+		bootstrap styles and functionality, just what's need for the gallery.
+	</div>
+</div>
