83f3c7b39d95caa2cbee83b3a7e87faa3abed75a
Erik <erikdmitchell@gmail.com>
2017-10-09 16:50:51 -0400

    moved admin file

--- pickle-cms.php
+++ pickle-cms.php
@@ -18,7 +18,7 @@ define('PICKLE_CMS_ADMIN_URL', plugin_dir_url(__FILE__).'admin/');
 define('PICKLE_CMS_VERSION', '0.1.0');
 
 require_once(PICKLE_CMS_PATH.'admin/functions.php'); // admin functions
-require_once(PICKLE_CMS_PATH.'admin.php'); // admin class
+require_once(PICKLE_CMS_PATH.'admin/admin.php'); // admin class
 //require_once(PICKLE_CMS_PATH.'admin/documentation/init.php'); // handles our documentation
 //require_once(PICKLE_CMS_PATH.'classes/admin-columns.php'); // allows for custom admin columns
 //require_once(PICKLE_CMS_PATH.'classes/taxonomies.php'); // calls custom taxonomies
